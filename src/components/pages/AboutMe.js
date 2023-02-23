f66e541e99d99dbf247a5ee687a7fc23c442e4fc
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-02-22 18:24:53 -0800

    AboutMe.js file is filled up. images folder is added to public folder to hold avatar image

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>Maksim Razuvaev</h1>
+      <h2>Full-Stack Web Developer</h2>
+      <img src="./public/images/avatar.png" alt="Avatar" class="avatar"></img>
+      <p>
+      I am a full-stack web developer. I am working on earning a certificate of Full Stack Codding Bootcamp - 
+      at the University of Washington, where I am developing skills in HTML5, CSS3, JavaScript, jQuery, Express.js, 
+      React.js, Node.js, progressive web apps, agile methodology, computer science, database theory, MongoDB, MySQL, 
+      Git, and more. 
+      </p>
+      <p>
+      As an innovative problem-solver passionate about developing apps, my current focus is on developing web applications with deep 
+      dives into the server and client-side API. Upon completing the bootcamp, I am going to continue my studies independently to learn 
+      more about Python, C#, and Amazon Web Services.
+      </p>
+    </div>
+  );
+}
