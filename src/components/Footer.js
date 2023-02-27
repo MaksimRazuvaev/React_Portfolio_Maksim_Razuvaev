21cef963e281e16676d89dbbf5c2a9c209437b7c
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-02-27 11:04:13 -0800

    Footer file is filled up with HTML

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 function Footer(){
     return(
-        <p>This is Footer</p>
+        <div >
+            <div >
+                <div >
+                    <p>Made with <span role="img" aria-label="heart icon">❤️</span> using React</p>
+                    <p>© Maksim Razuvaev</p>
+                </div>
+                <div >
+                    <p>Connect</p>
+                    <div>
+                        <a href="https://github.com/MaksimRazuvaev" target="_blank" rel="noopener noreferrer">
+                            <img title="github" alt="github" src="../../src/assets/images/GitHub.png" width="32px"></img></a>
+                        <a href="https://www.linkedin.com/in/maksim-razuvaev/" target="_blank" rel="noopener noreferrer">
+                            <img title="linkedin" alt="linkedin" src="../../src/assets/images/LinkedIn.svg" width="32px"></img></a>
+                        <a href="https://dev.to/maksimrazuvaev/" target="_blank" rel="noopener noreferrer">
+                            <img title="DEv" alt="DEV" src="../../src/assets/images/techDev.png" width="32px"></img></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// TODO: Add a comment explaining how we are able to extract the key value pairs from props
 
 function Header ({ currentPage, handlePageChange }) {
   return (
@@ -9,7 +8,6 @@ function Header ({ currentPage, handlePageChange }) {
         <a
           href="#AboutMe"
           onClick={() => handlePageChange('AboutMe')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
 
           className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
         >
@@ -20,7 +18,6 @@ function Header ({ currentPage, handlePageChange }) {
         <a
           href="#Contact"
           onClick={() => handlePageChange('Contact')}
-          //  TODO: Add a comment explaining what this logic is doing
 
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
@@ -31,7 +28,6 @@ function Header ({ currentPage, handlePageChange }) {
         <a
           href="#Portfolio"
           onClick={() => handlePageChange('Portfolio')}
-          //  TODO: Add a comment explaining what this logic is doing
 
           className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
@@ -41,7 +37,6 @@ function Header ({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#Resume"
-          //  TODO: Add a comment explaining what this logic is doing
 
           onClick={() => handlePageChange('Resume')}
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
